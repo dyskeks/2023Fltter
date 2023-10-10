e458d44047baab7ba8156eb7bcda2484baf60f69
sju1026 <wodnd565@naver.com>
2023-10-10 17:41:56 +0900

    ch3 done

--- ch3/lib/HelloPage.dart
+++ ch3/lib/HelloPage.dart
@@ -1,33 +1,55 @@
-import 'package:flutter/meterial.dart';
-import "HelloPage.dart";
+import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
-class HelloPage extends StatefulWidget{
+class Hellopage extends StatefulWidget{
   late String title;
-
+  int count = 0;
   HelloPage({super.key, String title}){
-    this.tilte = tilte;
+    this.title = title;
   }
 
-  State<HelloPage> createState() => _HelloPageState();
+ State<HelloPage> createState() => _HelloPageState();
 }
 
 class _HelloPageState() extends State<HelloPage> {
-String message = 'Hello World';
-Widget build(BuildContext context){
+  String massage = 'Hello World';
+
+  // int count = 0;
+
+  Widget build (BuildContext context){
   return Scaffold(
     appBar: AppBar(
-      title: Text('Hello Wold'),
+      title: Text(widget.title),
     ),
-    body: Text(
-      widget.title,
-      style: TextStyle(fontSize: 30),
-    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            massage,
+            style: TextStyle(fontSize: 30),
+          ),
+          Text(
+            '$widget.count',
+            style: TextStyle(fontSize: 50),
+
+          )
+        ],
+      ),
+    )
     floatingActionButton: FloatingActionButton(
       child: Icon(Icons.add),
-      onPressed: Changemessage,
+      onPressed: ChangeCounter,
     ),
   );
 }
+
+  void ChangeCounter(){
+
+    setState(() {
+      widget.count++;
+    });
+  }
   void Changemessage(){
     setState(() {
       message = "헬로 월드";
--- ch3/lib/main.dart
+++ ch3/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ch3/HelloPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
-      home: HelloPage()
+        title: "Flutter Demo",
+        home: HelloPage(title: "하이 월드",)
     );
   }
 }
+
 
